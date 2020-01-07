a36152871595e49cc15cf79f468a1e36cf53b34b
mattnorby <norbymatt@gmail.com>
2020-01-07 09:13:27 -0500

    Player character class and test

--- src/PlayerCharacter.java
+++ src/PlayerCharacter.java
@@ -1,2 +1,8 @@
 public class PlayerCharacter {
+    public PlayerCharacter(String name) {
+    }
+
+    public String getName() {
+        return null;
+    }
 }
--- src/PlayerCharacterTest.java
+++ src/PlayerCharacterTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlayerCharacterTest {
-
+    @Test
+    public void itHasAName() {
+        PlayerCharacter pc = new PlayerCharacter("Guy Royse");
+        assertEquals(pc.getName(), "Guy Royse");
+    }
 }
